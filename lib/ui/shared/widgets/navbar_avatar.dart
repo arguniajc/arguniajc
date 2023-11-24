3f4e7cae563688308874449c0c72cf16d66d0ff3
Diego Fernando Salazar Rosero <64756123+diegonandosalazar@users.noreply.github.com>
2023-11-24 09:21:23 -0500

    imagen de inicio sesion :

--- lib/ui/shared/widgets/navbar_avatar.dart
+++ lib/ui/shared/widgets/navbar_avatar.dart
@@ -1,15 +1,23 @@
+import 'package:control_actividades/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 // ignore: use_key_in_widget_constructors
 class NavbarAvatar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<AuthProvider>(context).usuario!;
+
     return ClipOval(
       child: SizedBox(
         width: 30,
         height: 30,
         child: Image.network(
-            'https://upload.wikimedia.org/wikipedia/commons/a/a4/Deportivo-cali-escudo.png'),
+          user.img ??
+              'https://upload.wikimedia.org/wikipedia/commons/c/c1/Sin_fotograf%C3%ADa.jpg',
+          fit: BoxFit
+              .cover, // Ajusta la imagen para cubrir completamente el espacio
+        ),
       ),
     );
   }
